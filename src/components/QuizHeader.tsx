deb5adcf81ab700e5c582150510bf26db5f0f257
jmsazz00 <jamesazzi193@gmail.com>
2024-11-15 14:57:57 +0200

    Updating quiz header design

--- src/components/QuizHeader.tsx
+++ src/components/QuizHeader.tsx
@@ -9,26 +9,29 @@ const QuizHeader = ({ name, year }: Props) => {
   return (
     <Box
       sx={{
-        my: 3,
+        mt: 2.5,
+        mb: 3,
         py: 1,
-        px: 2.5,
+        px: 3,
         borderRadius: 1,
         display: "flex",
         alignItems: "center",
         background:
-          "linear-gradient(90deg, rgba(88, 29, 104, 0.6), rgba(46, 31, 62, 0.3))",
+          "linear-gradient(90deg, rgba(88, 29, 104, 0.55), rgba(46, 31, 62, 0.7), rgba(18, 18, 18, 0.8))",
         width: "fit-content",
+        boxShadow: "0 2px 8px rgba(0, 0, 0, 0.2)",
       }}
     >
       <Typography
         variant="h5"
         sx={{
-          background: "linear-gradient(to right, #8e24aa, #bb86fc)",
+          background:
+            "linear-gradient(to right, rgba(142, 36, 170, 0.88), rgba(187, 134, 252, 0.66))",
           WebkitBackgroundClip: "text",
           WebkitTextFillColor: "transparent",
           fontWeight: "bold",
           borderRight: "2px solid #6a1b9a",
-          pr: 2,
+          pr: 2.5,
         }}
       >
         &lt; {name} Quiz
@@ -38,11 +41,11 @@ const QuizHeader = ({ name, year }: Props) => {
         sx={{
           color: "#bb86fc",
           fontSize: "0.75rem",
-          fontWeight: "600",
+          fontWeight: "500",
           backgroundColor: "#4a306b",
           borderRadius: 1,
           px: 1,
-          ml: 2,
+          ml: 2.5,
         }}
       >
         {year}
