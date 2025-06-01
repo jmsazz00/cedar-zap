b5fa373f23b37c0ea254fede59511a60245e9418
jmsazz00 <jamesazzi193@gmail.com>
2025-06-01 11:21:36 +0300

    Saving cardiology exams questions

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -2,7 +2,7 @@ import QuizLayout from "../components/QuizLayout";
 
 const QuizPage = () => {
   return (
-    <QuizLayout quiz={{ duration: 3600, name: "Gynecology", year: "24-25" }} />
+    <QuizLayout quiz={{ duration: 4800, name: "Gynecology", year: "24-25" }} />
   );
 };
 
